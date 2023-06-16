014f4c2b15ef9e20143e611e0153a55c100218d9
LoveWiz <loveckfanou@gmail.com>
2023-06-16 10:13:14 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,14 @@
  */
 int main(void)
 {
-    for (int i = 0; i <= 9; i++) {
-        for (int j = i + 1; j <= 9; j++) {
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = i + 1; j <= 9; j++)
+	{
             putchar('0' + i);
             putchar('0' + j);
-            if (i != 8 || j != 9) {
+            if (i != 8 || j != 9)
+	    {
                 putchar(',');
                 putchar(' ');
             }
