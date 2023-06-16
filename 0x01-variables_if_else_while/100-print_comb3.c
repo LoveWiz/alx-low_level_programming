be31b3120b06ddb2e0223d3aead64083ceeb140e
LoveWiz <loveckfanou@gmail.com>
2023-06-16 10:10:06 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all possible combinations of two-digit numbers.
+ * Numbers must be separated by commas and a space.
+ * The two digits must be different
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You can only use `putchar` to print to console
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+ */
+int main(void)
+{
+    for (int i = 0; i <= 9; i++) {
+        for (int j = i + 1; j <= 9; j++) {
+            putchar('0' + i);
+            putchar('0' + j);
+            if (i != 8 || j != 9) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+    return 0;
+}
