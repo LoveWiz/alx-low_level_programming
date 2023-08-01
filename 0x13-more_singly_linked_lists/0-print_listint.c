0c6a54895ed5746acd7f82263cbe5300cc19b527
LoveWiz <loveckfanou@gmail.com>
2023-08-01 16:07:32 +0000

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_listint - returns number of nodes
+ * @h: pointer to the head of linked list
+ * Return: number of of nodes
+*/
+
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
